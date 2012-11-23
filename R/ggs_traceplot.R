26b3b0ef079e00f8aa3fac2ba5634a6d462f9403
Xavier Fernández i Marín <xavier.fim@gmail.com>
2012-11-23 15:50:55 +0100

    Traceplot now shows by default the burnin period and takes care of the thinning

--- R/ggs_traceplot.R
+++ R/ggs_traceplot.R
@@ -2,15 +2,24 @@
 #'
 #' @param D data frame with the simulations
 #' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc). 
+#' @param original.burnin, logical. When true (default) start the Iteration counter in the x-axis at the end of the burnin period.
+#' @param original.thin, logical. When true (default) take into account the thinning interval in the x-axis.
 #' @return a ggplot object
 #' @export
 #' @examples
 #' data(samples)
 #' ggs_traceplot(ggs(S, parallel=FALSE))
-ggs_traceplot <- function(D, family=NA) {
+ggs_traceplot <- function(D, family=NA, original.burnin=TRUE, original.thin=TRUE) {
   # Manage subsetting a family of parameters
   if (!is.na(family)) {
     D <- get_family(D, family=family)
+  }
+  # Manage burnin
+  if (original.thin) {
+    D$Iteration <- ((D$Iteration-1) * attributes(D)$nThin) + 1
+  }
+  if (original.burnin) {
+    D$Iteration <- D$Iteration + attributes(D)$nBurnin
   }
   # Plot
   if (attributes(D)$nChains <= 1) {
