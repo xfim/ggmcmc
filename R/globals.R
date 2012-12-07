b5448c88e18ca3df4b775aa30a6a022185d46fa3
Xavier Fernández i Marín <xavier.fim@gmail.com>
2012-12-07 10:58:02 +0100

    Add fille globals.R with variables used in the package, to avoid R CMD check notes.

---
+++ R/globals.R
@@ -0,0 +1,15 @@
+# Hack to avoid NOTES in R CMD check
+# Hadley does not seem to like it: http://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+if (base::getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    "Parameter", "Chain", "value", "Iteration",   # from ggs object
+    "nIterations", # for ggs object attributes
+    "wa", "Rhat", # from ggs_Rhat
+    "Lag", "Autocorrelation", # from ggs_autocorrelation
+    "thick.low", "thick.high", "thin.low", "thin.high", # from ggs_caterpillar
+    "part_chain", # for ggs_compare_partial
+    "X1", "X2", # for ggs_crosscorrelation
+    "n", "z", "part", # for ggs_geweke
+    "m" # for ggs_geweke and ggs_running
+  ))
+}
