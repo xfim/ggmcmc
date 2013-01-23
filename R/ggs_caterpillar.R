f35ea7807a2f53a5b8cde21de96d07f4bb4c6c9f
Zachary M. Jones <zmj@zmjones.com>
2013-01-22 20:49:07 -0500

    added rocplot

--- R/ggs_caterpillar.R
+++ R/ggs_caterpillar.R
@@ -3,8 +3,8 @@
 #' Caterpillar plots are plotted combining all chains for each parameter.
 #'
 #' @param D Data frame whith the simulations or list of data frame with simulations. If a list of data frames with simulations is passed, the names of the models are the names of the objects in the list.
-#' @param X data frame with two columns, Parameter and the value for the x location. Parameter must be a character vector with the same names that the parameters in the D object. 
-#' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc). 
+#' @param X data frame with two columns, Parameter and the value for the x location. Parameter must be a character vector with the same names that the parameters in the D object.
+#' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc).
 #' @param thick.ci Vector of length 2 with the quantiles of the thick band for the credible interval
 #' @param thin.ci Vector of length 2 with the quantiles of the thin band for the credible interval
 #' @param line Numerical value indicating a concrete position, usually used to mark where zero is. By default do not plot any line.
@@ -16,10 +16,10 @@
 #' data(samples)
 #' ggs_caterpillar(ggs(S, parallel=FALSE))
 #' ggs_caterpillar(list(A=ggs(S, parallel=FALSE), B=ggs(S, parallel=FALSE))) # silly example duplicating the same model
-ggs_caterpillar <- function(D, family=NA, X=NA, 
+ggs_caterpillar <- function(D, family=NA, X=NA,
   thick.ci=c(0.05, 0.95), thin.ci=c(0.025, 0.975),
   line=NA, horizontal=TRUE, labels=NULL) {
-  
+
   # Manage subsetting a family of parameters
   if (!is.na(family)) {
     D <- get_family(D, family=family)
@@ -47,9 +47,9 @@ ggs_caterpillar <- function(D, family=NA, X=NA,
   # http://stackoverflow.com/questions/6955128/object-not-found-error-with-ddply-inside-a-function
   # One of the solutions, not elegant, is to assign qs globally (as well as
   # locally  for further commands in this function
-  qs  <- qs <<- c(thin.low=thin.ci[1], thick.low=thick.ci[1], 
+  qs  <- qs <<- c(thin.low=thin.ci[1], thick.low=thick.ci[1],
                   median=0.5, thick.high=thick.ci[2], thin.high=thin.ci[2])
-  
+
   # Multiple models or a single model
   #
   if (!is.data.frame(D)) { # D is a list, and so multiple models are passed
@@ -65,7 +65,7 @@ ggs_caterpillar <- function(D, family=NA, X=NA,
     model.names <- NA
     # Check that a vector of labels for the models has been passed, or that the
     # list has named ggs elements, so that the model has really a name
-    if ( length(labels)!=length(D) & length(names(D))==0 ) { 
+    if ( length(labels)!=length(D) & length(names(D))==0 ) {
       stop("Label length does not match the model list length, or the list of models is not named.")
     }
     # Prevalence is for the names of the named list, not for labels
@@ -73,13 +73,13 @@ ggs_caterpillar <- function(D, family=NA, X=NA,
     if (length(names(D)!=0)) model.names <- names(D)           # get model names from named list
     # Final data frame to use for plotting
     dcm <- do.call(
-      rbind, 
-      lapply(1:length(D), 
+      rbind,
+      lapply(1:length(D),
         function(i) if (length(D[[i]]) > 1) cbind(D[[i]], Model=model.names[i])))
 
   } else if (is.data.frame(D)) { # D is a data frame, and so a single model is passed
     multi <-  FALSE
-    dc <- ddply(D, .(Parameter), summarize, 
+    dc <- ddply(D, .(Parameter), summarize,
                 q=quantile(value, probs=qs), qs=qs,
                 .parallel=attributes(D)$parallel)
     dc$qs <- factor(dc$qs, labels=names(qs))
@@ -104,11 +104,11 @@ ggs_caterpillar <- function(D, family=NA, X=NA,
 
   # Manage horizontal or vertical plot
   if (horizontal == FALSE) {
-    f <- f + coord_flip() 
+    f <- f + coord_flip()
   }
 
   # Manage multiple models
-  if (multi==TRUE & horizontal==TRUE) 
+  if (multi==TRUE & horizontal==TRUE)
     f <- f + facet_grid(Model ~ ., scale="free", space="free")
   if (multi==TRUE & horizontal==FALSE)
     f <- f + facet_grid(. ~ Model, scale="free", space="free")
@@ -118,7 +118,7 @@ ggs_caterpillar <- function(D, family=NA, X=NA,
     f <- f + theme(legend.position="none", axis.text.x=element_text(size=7, hjust=1, angle=90))
   } else {
     f <- f + theme(legend.position="none", axis.text.x=element_text(size=7, hjust=1))
-  } 
+  }
 
   # Add a line to remark a specific point
   if (!is.na(line)) {
