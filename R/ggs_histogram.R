97baf4c006e4bc9ac3dbe07e9c4e778ad544b2b8
Zachary M. Jones <zmj@zmjones.com>
2013-03-25 21:42:13 -0400

    added default binwidth to ggs_histogram

--- R/ggs_histogram.R
+++ R/ggs_histogram.R
@@ -3,19 +3,20 @@
 #' Plot a histogram of each of the parameters. Histograms are plotted combining all chains for each parameter.
 #
 #' @param D Data frame whith the simulations.
-#' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc). 
+#' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc).
+#' @param binwidth Number of bins in the histogram, defaults to 30.
 #' @return A \code{ggplot} object.
 #' @export
 #' @examples
 #' data(samples)
 #' ggs_histogram(ggs(S, parallel=FALSE))
-ggs_histogram <- function(D, family=NA) {
+ggs_histogram <- function(D, family=NA, binwidth = 30) {
   # Manage subsetting a family of parameters
   if (!is.na(family)) {
     D <- get_family(D, family=family)
   }
   # Plot
-  f <- ggplot(D, aes(x=value)) + geom_histogram() + 
+  f <- ggplot(D, aes(x=value)) + geom_histogram(binwidth = binwidth) +
     facet_wrap(~ Parameter, ncol=1, scales="free")
   return(f)
 }
