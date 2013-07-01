e645a7047e66e21384bca7d8c92a6648427a8133
Xavier Fernández i Marín <xavier.fim@gmail.com>
2013-07-01 08:17:54 +0200

    Code arrangement.

--- R/ggs_crosscorrelation.R
+++ R/ggs_crosscorrelation.R
@@ -20,7 +20,7 @@ ggs_crosscorrelation <- function(D, family=NA, absolute.scale=TRUE) {
   } 
   X <- dcast(D, Iteration + Chain ~ Parameter)
   # Chain management is not easy
-  bc.cc <- melt(cor(as.matrix(X[,-c(1, 2),drop=FALSE])))
+  bc.cc <- melt(cor(as.matrix(X[,-c(1, 2), drop=FALSE])))
   # Need to revert parameter names
   bc.cc$Var1 <- factor(bc.cc$Var1, labels=levels(D$Parameter))
   bc.cc$Var2 <- factor(bc.cc$Var2, labels=levels(D$Parameter))
