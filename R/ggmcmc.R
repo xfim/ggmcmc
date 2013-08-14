04d7f4180ed1d38ad7140d9433660b4dca2bd7ec
Xavier Fernández i Marín <xavier.fim@gmail.com>
2013-08-14 15:52:27 +0100

    ggmcmc() accepts "plot" as an argument, indicating whether only some of the partial plots are desired.
    In addition to that, caterpillar plots are produced with all parameters by
    default, and not by families, as the "family" argument since 0.4 is much more
    versatile and allows for more flexible specifications.

--- R/ggmcmc.R
+++ R/ggmcmc.R
@@ -7,6 +7,7 @@
 #' @param D Data frame whith the simulations, previously arranged using \code{\link{ggs}}
 #' @param file Character vector with the name of the file to create. Defaults to "ggmcmc-output.pdf". When NULL, no pdf device is opened or closed. This allows the user to work with an opened pdf (or other) device.
 #' @param family Name of the family of parameters to plot, as given by a character vector or a regular expression. A family of parameters is considered to be any group of parameters with the same name but different numerical value between square brackets (as beta[1], beta[2], etc). 
+#' @param plot character vector containing the names of the desired plots. By default (NULL), \code{ggmcmc()} plots \code{ggs_histogram()}, \code{ggs_density()}, \code{ggs_traceplot()}, \code{ggs_running()}, \code{ggs_compare_partial()}, \code{ggs_autocorrelation()}, \code{ggs_crosscorrelation()}, \code{ggs_Rhat()}, \code{ggs_geweke()} and \code{ggs_caterpillar()}.
 #' @param param.page Numerical, number of parameters to plot for each page. Defaults to 5.
 #' @param width Width of the pdf display, in inches. Defaults to 7.
 #' @param height Height of the pdf display, in inches. Defaults to 10.
@@ -15,7 +16,8 @@
 #' @examples
 #' data(samples)
 #' ggmcmc(ggs(S))  # Directly from a coda object
-ggmcmc <- function(D, file="ggmcmc-output.pdf", family=NA, param.page=5, width=7, height=10, ...) {
+ggmcmc <- function(D, file="ggmcmc-output.pdf", family=NA, plot=NULL,
+  param.page=5, width=7, height=10, ...) {
   # Manage subsetting a family of parameters
   if (!is.na(family)) {
     D <- get_family(D, family=family)
@@ -34,23 +36,35 @@ ggmcmc <- function(D, file="ggmcmc-output.pdf", family=NA, param.page=5, width=7
   # arranged
   if (attributes(D)$nParameters <= param.page) {
     # Simply print each plot separately
-    cat("Plotting histograms\n")
-    print(ggs_histogram(D))
+    if (is.null(plot) | length(grep("histogram", plot)) > 0) {
+      cat("Plotting histograms\n")
+      print(ggs_histogram(D))
+    }
 
-    cat("Plotting density plots\n")
-    print(ggs_density(D))
+    if (is.null(plot) | length(grep("density", plot)) > 0) {
+      cat("Plotting density plots\n")
+      print(ggs_density(D))
+    }
 
-    cat("Plotting traceplots\n")
-    print(ggs_traceplot(D))
+    if (is.null(plot) | length(grep("traceplot", plot)) > 0) {
+      cat("Plotting traceplots\n")
+      print(ggs_traceplot(D))
+    }
 
-    cat("Plotting running means\n")
-    print(ggs_running(D))
+    if (is.null(plot) | length(grep("running", plot)) > 0) {
+      cat("Plotting running means\n")
+      print(ggs_running(D))
+    }
 
-    cat("Plotting comparison of partial and full chain\n")
-    print(ggs_compare_partial(D))
+    if (is.null(plot) | length(grep("compare_partial", plot)) > 0) {
+      cat("Plotting comparison of partial and full chain\n")
+      print(ggs_compare_partial(D))
+    }
 
-    cat("Plotting autocorrelation plots\n")
-    print(ggs_autocorrelation(D))
+    if (is.null(plot) | length(grep("autocorrelation", plot)) > 0) {
+      cat("Plotting autocorrelation plots\n")
+      print(ggs_autocorrelation(D))
+    }
 
   } else {
     # Arrange manually the plots to fit in the pages
@@ -77,27 +91,39 @@ ggmcmc <- function(D, file="ggmcmc-output.pdf", family=NA, param.page=5, width=7
     #ddply(D, .(page), .fun=ggs_histogram(D))
 
     # So just do it manually
-    cat("Plotting histograms\n")
-    for (p in 1:n.pages) {
-      Dsub <- D[D$page==p,]
-      attr(Dsub, "nParameters") <- length(unique(Dsub$Parameter))
-      print(ggs_histogram(Dsub))
+    if (is.null(plot) | length(grep("histogram", plot)) > 0) {
+      cat("Plotting histograms\n")
+      for (p in 1:n.pages) {
+        Dsub <- D[D$page==p,]
+        attr(Dsub, "nParameters") <- length(unique(Dsub$Parameter))
+        print(ggs_histogram(Dsub))
+      }
     }
 
-    cat("Plotting density plots\n")
-    for (p in 1:n.pages) print(ggs_density(D[D$page==p,]))
+    if (is.null(plot) | length(grep("density", plot)) > 0) {
+      cat("Plotting density plots\n")
+      for (p in 1:n.pages) print(ggs_density(D[D$page==p,]))
+    }
 
-    cat("Plotting traceplots\n")
-    for (p in 1:n.pages) print(ggs_traceplot(D[D$page==p,]))
+    if (is.null(plot) | length(grep("traceplot", plot)) > 0) {
+      cat("Plotting traceplots\n")
+      for (p in 1:n.pages) print(ggs_traceplot(D[D$page==p,]))
+    }
 
-    cat("Plotting running means\n")
-    for (p in 1:n.pages) print(ggs_running(D[D$page==p,]))
+    if (is.null(plot) | length(grep("running", plot)) > 0) {
+      cat("Plotting running means\n")
+      for (p in 1:n.pages) print(ggs_running(D[D$page==p,]))
+    }
 
-    cat("Plotting comparison of partial and full chain\n")
-    for (p in 1:n.pages) print(ggs_compare_partial(D[D$page==p,]))
+    if (is.null(plot) | length(grep("compare_partial", plot)) > 0) {
+      cat("Plotting comparison of partial and full chain\n")
+      for (p in 1:n.pages) print(ggs_compare_partial(D[D$page==p,]))
+    }
 
-    cat("Plotting autocorrelation plots\n")
-    for (p in 1:n.pages) print(ggs_autocorrelation(D[D$page==p,]))
+    if (is.null(plot) | length(grep("autocorrelation", plot)) > 0) {
+      cat("Plotting autocorrelation plots\n")
+      for (p in 1:n.pages) print(ggs_autocorrelation(D[D$page==p,]))
+    }
 
   }
 
@@ -105,33 +131,27 @@ ggmcmc <- function(D, file="ggmcmc-output.pdf", family=NA, param.page=5, width=7
   ## Print also the figures that can fit in a single page
   ##
   if (attributes(D)$nParameters > 1) {                    # only in case of more than one parameter
-    cat("Plotting crosscorrelation plot\n")
-    print(ggs_crosscorrelation(D))
+    if (is.null(plot) | length(grep("crosscorrelation", plot)) > 0) {
+      cat("Plotting crosscorrelation plot\n")
+      print(ggs_crosscorrelation(D))
+    }
   }
 
   if (attributes(D)$nChain > 1) {                         # only in case of multiple chains
-    cat("Plotting Potential Scale Reduction Factors\n")
-    print(ggs_Rhat(D))
+    if (is.null(plot) | length(grep("Rhat", plot)) > 0) {
+      cat("Plotting Potential Scale Reduction Factors\n")
+      print(ggs_Rhat(D))
+    }
   }
 
-  cat("Plotting Geweke Diagnostic\n")
-  print(ggs_geweke(D))
+  if (is.null(plot) | length(grep("geweke", plot)) > 0) {
+    cat("Plotting Geweke Diagnostic\n")
+    print(ggs_geweke(D))
+  }
 
-  ##
-  ## Print caterpillar plots for each of the repeated parameters
-  ##
-  cat("Plotting caterpillar plot\n")
-
-  # get the names of the parameters without their number (parameter family)
-  Parameter.family <- gsub("\\[.+\\]", "", D$Parameter)
-
-  # Count how many members each family of parameters has, and only plot where
-  # there is more than one parameter
-  n.family.members <- apply(ifelse(table(D$Parameter, Parameter.family) > 0, 1, 0), 2, sum)
-  for (f in unique(Parameter.family)) {
-    if (n.family.members[f] > 1) {
-      print(ggs_caterpillar(D, family=f, horizontal=TRUE) + labs(title=f))
-    }
+  if (is.null(plot) | length(grep("caterpillar", plot)) > 0) {
+    cat("Plotting caterpillar plot\n")
+    print(ggs_caterpillar(D))
   }
 
   # Close the pdf device
